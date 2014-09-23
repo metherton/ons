655643748f191a4043d26ae6083802da4168e991
metherton <martinetherton63@googlemail.com>
2014-09-23 08:55:07 +0200

    modfieed mapping

--- ons-command/src/main/webapp/angular/app/js/controllers.js
+++ ons-command/src/main/webapp/angular/app/js/controllers.js
@@ -1,3 +1,12 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
+  $http.get('http://localhost:8080/ons-command/rest/persons/1').success(function(data) {
+    $scope.persons = data;
+  });
+
+});
