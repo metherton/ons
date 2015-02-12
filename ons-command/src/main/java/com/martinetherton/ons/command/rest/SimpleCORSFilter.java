9373a9da0d774e11b1b047f0a37d45bf5d53b858
metherton <martinetherton63@googlemail.com>
2015-02-12 23:54:28 +0100

    cirs

--- ons-command/src/main/java/com/martinetherton/ons/command/rest/SimpleCORSFilter.java
+++ ons-command/src/main/java/com/martinetherton/ons/command/rest/SimpleCORSFilter.java
@@ -21,16 +21,26 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        String origin = request.getHeader("Origin");
+      //  response.addHeader("Access-Control-Allow-Origin", "MACBOOKPRO-F7A9");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.addHeader("Access-Control-Allow-Headers", "Origin");
+        response.addHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin");
+        response.addHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials");
+
+        response.setHeader("Access-Control-Allow-Origin", origin);
+        response.setHeader("Vary", "Origin");
+   //     Vary: Accept-Encoding, Origin
         if (request.getMethod() != "OPTIONS" ) {
             chain.doFilter(req, res);
-        } else {
-        }
+       } else {
+
+       }
 
 
     }
